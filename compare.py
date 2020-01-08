0039d06bcfc203e5ec9ae78c3c6580ea985744ce
CHy-KK <45690293+CHy-KK@users.noreply.github.com>
2020-01-08 14:24:36 +0800

    Create compare.py

---
+++ compare.py
@@ -0,0 +1,81 @@
+def convert_single_example(ex_index, example, label_list, max_seq_length, tokenizer, mode):
+    """
+    :param ex_index: example num
+    :param example:
+    :param label_list: all labels
+    :param max_seq_length:
+    :param tokenizer: WordPiece tokenization
+    :param mode:
+    :return: feature
+
+    IN this part we should rebuild input sentences to the following format.
+    example:[Jim,Hen,##son,was,a,puppet,##eer]
+    labels: [I-PER,I-PER,X,O,O,O,X]
+
+    """
+    label_map = {}
+    #here start with zero this means that "[PAD]" is zero
+    for (i,label) in enumerate(label_list):
+        label_map[label] = i
+    with open(FLAGS.middle_output+"/label2id.pkl",'wb') as w:
+        pickle.dump(label_map,w)
+    textlist = example.text.split(' ')
+    labellist = example.label.split(' ')
+    tokens = []
+    labels = []
+    for i,(word,label) in enumerate(zip(textlist,labellist)):
+        token = tokenizer.tokenize(word)
+        tokens.extend(token)
+        for i,_ in enumerate(token):
+            if i==0:
+                labels.append(label)
+            else:
+                labels.append("X")
+    # only Account for [CLS] with "- 1".
+    if len(tokens) >= max_seq_length - 1:
+        tokens = tokens[0:(max_seq_length - 1)]
+        labels = labels[0:(max_seq_length - 1)]
+    ntokens = []
+    segment_ids = []
+    label_ids = []
+    ntokens.append("[CLS]")
+    segment_ids.append(0)
+    label_ids.append(label_map["[CLS]"])
+    for i, token in enumerate(tokens):
+        ntokens.append(token)
+        segment_ids.append(0)
+        label_ids.append(label_map[labels[i]])
+    # after that we don't add "[SEP]" because we want a sentence don't have
+    # stop tag, because i think its not very necessary.
+    # or if add "[SEP]" the model even will cause problem, special the crf layer was used.
+    input_ids = tokenizer.convert_tokens_to_ids(ntokens)
+    mask = [1]*len(input_ids)
+    #use zero to padding and you should
+    while len(input_ids) < max_seq_length:
+        input_ids.append(0)
+        mask.append(0)
+        segment_ids.append(0)
+        label_ids.append(0)
+        ntokens.append("[PAD]")
+    assert len(input_ids) == max_seq_length
+    assert len(mask) == max_seq_length
+    assert len(segment_ids) == max_seq_length
+    assert len(label_ids) == max_seq_length
+    assert len(ntokens) == max_seq_length
+    if ex_index < 3:
+        logging.info("*** Example ***")
+        logging.info("guid: %s" % (example.guid))
+        logging.info("tokens: %s" % " ".join(
+            [tokenization.printable_text(x) for x in tokens]))
+        logging.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
+        logging.info("input_mask: %s" % " ".join([str(x) for x in mask]))
+        logging.info("segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
+        logging.info("label_ids: %s" % " ".join([str(x) for x in label_ids]))
+    feature = InputFeatures(
+        input_ids=input_ids,
+        mask=mask,
+        segment_ids=segment_ids,
+        label_ids=label_ids,
+    )
+    # we need ntokens because if we do predict it can help us return to original token.
+    return feature,ntokens,label_ids
